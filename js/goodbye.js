4200c2081611d9d74a2373baefe0cd2b532a7ad2
AsifImran001 <19011519-134@uog.edu.pk>
2020-07-19 12:16:44 +0500

    first commit

---
+++ js/goodbye.js
@@ -0,0 +1,14 @@
+(function (window) {
+   1
+    var byeSpeaker = {};
+    
+    // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+    var speakWord = "Good Bye";
+    
+    byeSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+    }
+    
+    window.byeSpeaker = byeSpeaker;
+    
+    })(window);
